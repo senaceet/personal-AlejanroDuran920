a4845a05b2118f3c41939688320e9ba127cc49c1
alejandro duran <aaduran08@misena.edu.co>
2019-10-14 19:43:13 -0500

    docs: se adjuntan  programas python

---
+++ 2.Desarrollo/programas python/gradosselsius.py
@@ -0,0 +1,9 @@
+print("bienvenido")
+print("ingrese los grados a convertir")
+a=float(input(""))
+b=(a*1.8)+32
+print("la conversion a grados farenhein es: ",b)
+k=(a+273.15)
+print("La conversion a grados kelvin es: ",k)
+r=(a*9/5)+491.67
+print("La conversion a grados rankine es: ",r)
---
+++ 2.Desarrollo/programas python/hola_mundo.py
@@ -0,0 +1,5 @@
+a = int(input("ingrese su numero:  "))
+b = a**0.5
+c =(b)
+print  ("La raiz de su numero es: ", b) 
+print("Fin del programa")
