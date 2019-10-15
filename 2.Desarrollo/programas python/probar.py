336b9fabefc5cf84f75ddee179b6aa75605dbd68
alejandro duran <aaduran08@misena.edu.co>
2019-10-14 19:44:29 -0500

    docs: se adjunta programa python

---
+++ 2.Desarrollo/programas python/probar.py
@@ -0,0 +1,26 @@
+print("python"[4])# imprime o
+print("hello python!"[-9])# imprime o
+print("python"[0:3])#imprime pyt
+print("python"[:2])# imprime las dos primeras letras de la palabra
+print("python"[-3:-2])#imprime la cuarta letra hacia la derecha y la 3 hacia la izquierda
+print("python"[2:-2])#imprime la tercera letra hacia la derecha de la palabra y la 3 letra hacia la derecha de la palabra
+print("python"[:])#imprime toda la palabra en su contenido
+print("python".find("tho"))#imprime el numero 2
+print("python".find("oh"))#imprime el numero -1
+print("whizzbuzz".rfind("zz"))#imprime el numero 7
+print("python".lstrip(""))#imprime la palabra
+print("smallElements".capitalize())#imprime la palabra en minusculas
+print("pyThOn".swapcase())#imprime las letras de la palabra en mayusculas
+print("python"[-2])#imprime las letras hacia la izquierda
+print("python"[5])#imprime la poscicion 5 de la palabra
+print("python"[2:2])
+print("python"[2:])#imprime la omicion de las 2 primeras posciciones
+print("python"[-5:-1])#imprime las posiciones indicadas
+print("python"[-4:4])#imprime las posiciones indicadas en el vector
+print("python"[-10:10])#imprime todo lo que hay entre el rango de entre menos 10 y 10
+print("python".find("ty"))#imprime el numero -1
+print("python".find("pyt"))#imprime el numero 0
+print("python".find("zz"))#imprime el numero -1
+print("hello_world".startswith("hell"))#imprime verdadero
+print("hello_python".rpartition('_'))#imprime lo que esta dentro de las comillas
+print("python/java/c++".split('/'))#imprime lo que esta dentro de los parentesis exceptuando los slahs y convirtiendolos en ,
