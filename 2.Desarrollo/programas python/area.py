3e9d0b0223452b182bd93ae6e59376b09dd6c050
alejandro duran <aaduran08@misena.edu.co>
2019-10-14 19:41:26 -0500

    docs: se adjunta programa de python

---
+++ 2.Desarrollo/programas python/area.py
@@ -0,0 +1,21 @@
+print("digite :")
+print ("1 si desea obtener el area del  triangulo")
+print ("2 si desea obtener el area del  rectangulo")
+print ("3 si desea obtener el area del cuadrado")
+print ("4 si desea obtener el area del circulo ")
+print ("5 si desea obtener el area del losango")
+print ("6 si desea obtener el area del trapezio")
+x = int(input(""))
+if (x == 1):
+     print("Area del triangulo")
+     b=float(input("digite la base "))
+     h= float (input("digite la altura"))
+     op = b*h/2
+print("El area de su triangulo es: ", op)
+elif ()
+    print("Area del rectangulo")
+
+            
+    
+
+   
